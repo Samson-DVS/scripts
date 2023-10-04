24c0e8984b8146b73064671725f4b931622b1bad
Samson <57755235+Samson-DVS@users.noreply.github.com>
2023-10-04 10:25:19 +0800

    Create mern.sh

---
+++ mern.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#Author: Visahl Samson David Selvam
+# Update the package list
+sudo apt update
+# Install curl if it's not already installed
+sudo apt install -y curl
+# Install the latest version of Node.js and npm using NodeSource
+curl -fsSL https://deb.nodesource.com/setup_current.x | sudo -E bash -
+sudo apt-get install -y nodejs
+# Install the latest version of npm
+sudo npm install -g npm
+# Install Yarn
+sudo npm install -g yarn
+# Verify the installation
+node -v
+npm -v
+yarn --version
